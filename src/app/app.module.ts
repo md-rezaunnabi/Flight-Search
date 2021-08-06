cc55364073a9ab0e90b45b0712a7d7317ad82a59
Md. Rezaunnabi <rezaunnabi@gmail.com>
2021-08-06 12:51:31 +0600

    added transloco for localisation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TranslocoRootModule } from './transloco/transloco-root.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, TranslocoRootModule],
   providers: [],
   bootstrap: [AppComponent]
 })
