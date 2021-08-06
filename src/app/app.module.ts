99ffc004926269b85f5e30bc2d2578bf977b5faf
Md. Rezaunnabi <rezaunnabi@gmail.com>
2021-08-06 13:26:40 +0600

    added header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './layout/header/header.component';
 import { TranslocoRootModule } from './transloco-root.module';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HeaderComponent],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule, TranslocoRootModule],
   providers: [],
   bootstrap: [AppComponent]
